da3eaeff101f8da0d2ed103ba0512dee39b6b2b7
LiangYong <517219382@qq.com>
2020-04-18 17:45:53 +0800

    first commit

---
+++ src/main/java/com/example/k8sdemoapp/K8sDemoAppApplication.java
@@ -0,0 +1,20 @@
+package com.example.k8sdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class K8sDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sDemoAppApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/example/k8sdemoapp/K8sDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8sdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
